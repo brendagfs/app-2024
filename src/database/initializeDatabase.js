574f463413bbf7852a15c732b95e861899ff4440
brendagfs <brendagdsantos@gmail.com>
2024-11-25 21:19:59 +0000

    [feat] mocl data users

--- src/database/initializeDatabase.js
+++ src/database/initializeDatabase.js
@@ -1,9 +1,13 @@
 export async function initializeDatabase(database) {
-  try {
-      await database.execAsync(`
-          DROP TABLE IF EXISTS payments;
+    try {
+        await database.execAsync(`
+         /* DROP TABLE IF EXISTS payments;
           
           DROP TABLE IF EXISTS users;
+
+          DROP INDEX IF EXISTS idx_users_nome;
+
+          DROP INDEEX IF EXISTS idx_payments_data_pagamento; */
 
           CREATE TABLE IF NOT EXISTS users (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -21,6 +25,7 @@ export async function initializeDatabase(database) {
                 user_cadastro INTEGER NOT NULL,
                 valor_pago REAL NOT NULL,
                 data_pagamento DATE NOT NULL,
+                numero_recibo TEXT NOT NULL,
                 observacao TEXT,
                 created_at DATE DEFAULT CURRENT_TIMESTAMP,
                 update_at DATE,
@@ -29,15 +34,21 @@ export async function initializeDatabase(database) {
                 temporada TEXT,
                 descricao TEXT,
                 FOREIGN KEY (user_id) REFERENCES users(id),
-                FOREIGN KEY (user_nome) REFERENCES users(nome)
+                FOREIGN KEY (user_cadastro) REFERENCES users(id)
           );
 
-          INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Super','super@email.com','A123456a!','SUPER');
-          INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Admin','admin@email.com','A123456a!','ADMIN');
-          INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('User','user@email.com','A123456a!','USER');
+          CREATE INDEX IF NOT EXISTS idx_users_nome ON users (nome);
 
+         CREATE INDEX IF EXISTS idx_payments_data_pagamentos ON payments (date_pagamento);
+          
+         /* INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Super','super@email.com','A123456a!','SUPER');
+          INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Admin','admin@email.com','A123456a!','ADMIN');
+          INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('User','user@email.com','A123456a!','USER'); */
+
+          
           `);
-  } catch (error) {
-      console.log(error);
-  }
+    } catch (error) {
+        console.log(error);
+    }
 }
+ 
